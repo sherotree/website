dbb84f286909102d0600c9ad9cc239b59d7d355e
Zero <mobius_pan@yeah.net>
2021-05-06 11:53:10 +0800

    fix phoneNo regular (#19)

--- src/common/utils/reg-rules.ts
+++ src/common/utils/reg-rules.ts
@@ -14,11 +14,8 @@
 
 import i18n from '~/i18n';
 
-/**
- * Created by 含光<jiankang.pjk@alibaba-inc.com> on 2021/3/3 17:58.
- */
 const RegularMap = {
-  mobile: { pattern: /^(1[3|4|5|7|8|9])\d{9}$/, message: i18n.t('please enter the correct phone number') },
+  mobile: { pattern: /^(1[3|4|5|6|7|8|9])\d{9}$/, message: i18n.t('please enter the correct phone number') },
   email: { pattern: /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/, message: i18n.t('please enter the correct email') },
 };
 
